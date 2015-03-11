aa61a519d6abf56df583ebd0250e33dcdbedeed8
fitnr <fitnr@fakeisthenewreal>
2015-03-11 14:10:14 -0400

    v0.2.0

--- bin/yaml-cat.js
+++ bin/yaml-cat.js
@@ -8,7 +8,7 @@ var yamlcat = require('..');
 function toLowerCase(val) { return String.prototype.toLowerCase.apply(val); }
 
 program
-    .version('0.1.5')
+    .version('0.2.0')
     .usage('[options] <file ...>')
     .description('Concatenate the YAML front matter of several files')
     .option('-o, --output <file>', 'Save result to file', String, '-')
